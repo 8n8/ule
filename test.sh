a584669ce661cf154db6a17679b93bbeb4505127
True Ghiassi <true@ghiassitrio.co.uk>
2024-08-02 21:07:44 +0100

    Another snapshot test, using github.com/rtfeldman/elm-spa-example
    The test uses Ule to build the application and then compares the
    resulting Javascript file with one created by Elm 0.19.1.

--- test.sh
+++ test.sh
@@ -4,10 +4,15 @@ set -e
 
 stack build --fast
 
-rm -rf test/input/helloWorld/elm-stuff
+function snapshot_test() {
+    rm -rf "test/input/$1/elm-stuff"
 
-pushd test/input/helloWorld
-stack exec ule -- make src/Main.elm --output=elm.js
-popd
+    pushd test/input/$1
+    stack exec ule -- make src/Main.elm --output=elm.js
+    popd
 
-diff test/input/helloWorld/elm.js test/expected/helloWorld.js
+    diff "test/input/$1/elm.js" "test/expected/$1.js"
+}
+
+snapshot_test helloWorld
+snapshot_test elm-spa-example
