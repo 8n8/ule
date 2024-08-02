d43eb05e278089adc217f6cb1cfa613816d1b4e1
True Ghiassi <true@ghiassitrio.co.uk>
2024-08-02 21:34:59 +0100

    Snapshot tests also compare optimized builds
    Previously they only compared debug builds.

--- test.sh
+++ test.sh
@@ -4,14 +4,27 @@ set -e
 
 stack build --fast
 
-function snapshot_test() {
-    rm -rf "test/input/$1/elm-stuff"
-
+function debug_test() {
     pushd test/input/$1
     stack exec ule -- make src/Main.elm --output=elm.js
     popd
 
-    diff "test/input/$1/elm.js" "test/expected/$1.js"
+    diff "test/input/$1/elm.js" "test/expected/debug/$1.js"
+}
+
+function optimize_test() {
+    pushd test/input/$1
+    stack exec ule -- make src/Main.elm --optimize --output=elm.js
+    popd
+
+    diff "test/input/$1/elm.js" "test/expected/optimize/$1.js"
+}
+
+function snapshot_test() {
+    rm -rf "test/input/$1/elm-stuff"
+
+    debug_test "$1"
+    optimize_test "$1"
 }
 
 snapshot_test helloWorld
